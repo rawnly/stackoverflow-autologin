cb467250f8609aa34ea64388b5fe706b148a69dd
Rawnly <fedevitale99@gmail.com>
2021-01-22 10:37:17 +0000

    fix

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ with open("./config.toml", mode="r") as config_file:
     config = toml.load(config_file)
 
 # Create the connection 
-connection = StackOverflow(config["user"]["email"], configp["user"]["password"], config["notifications"]["webhook"])
+connection = StackOverflow(config["user"]["username"], config["user"]["password"], config["notifications"]["webhook"])
 
 # Login and check if the user is logged
 connection.login()
@@ -14,9 +14,9 @@ isLogged = connection.logged()
 # if logged print
 # else display an error
 if isLogged:
-    print 'StackOverflow: Logged'
+    print('StackOverflow: Logged')
 else:
-    print 'StackOverflow: Can\'t login.. try again later.'
+    print('StackOverflow: Can\'t login.. try again later.')
 
 # close the session
 connection.close()
